708ef696a2e45dac72afcc3a35f12d715f4571ae
Christian Maddox <cmaddox5@vols.utk.edu>
2016-12-24 14:34:19 +0000

    Added a name field to users and winning_player field to games.

--- db/migrate/20161221001406_create_games.rb
+++ db/migrate/20161221001406_create_games.rb
@@ -3,6 +3,7 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.string :status
       t.string :title
+      t.references :winning_player, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221012310) do
+ActiveRecord::Schema.define(version: 20161224143102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,12 @@ ActiveRecord::Schema.define(version: 20161221012310) do
   create_table "games", force: true do |t|
     t.string   "status"
     t.string   "title"
+    t.integer  "winning_player_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "games", ["winning_player_id"], name: "index_games_on_winning_player_id", using: :btree
 
   create_table "pieces", force: true do |t|
     t.integer  "x_pos"
@@ -59,6 +62,7 @@ ActiveRecord::Schema.define(version: 20161221012310) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
