970d7af3f30c469bac5151d8d1834c16f932d22e
Chris Choi <cdddchris@gmail.com>
2016-12-26 21:03:41 +0000

    Used Christian's Piece class as super piece, then created all game pieces and added descriptions of legal moves with piece

---
+++ app/models/knight.rb
@@ -0,0 +1,5 @@
+class Knight < Piece
+
+  # Knights move in a very different way from the other pieces – going two squares in one
+  # direction, and then one more move at a 90 degree angle, just like the shape of an “L”.
+  # Knights are also the only pieces that can move over other pieces.
---
+++ app/models/queen.rb
@@ -0,0 +1,3 @@
+class Queen < Piece
+  # She can move in any one straight direction - forward, backward, sideways,
+  # or diagonally - as far as possible as long as she does not move through any of her own pieces.
