12929253400e74e272915e316163ede7d2a90c55
Christian Maddox <cmaddox5@vols.utk.edu>
2016-12-28 20:23:37 +0000

    Added a validation so a game can only be created if at least one player id is associated with it.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
+  validates :white_player_id, :presence => true, :if => "black_player_id.nil?"
+  validates :black_player_id, :presence => true, :if => "white_player_id.nil?"
   has_many :pieces
   belongs_to :white_player, class_name: 'User'
   belongs_to :black_player, class_name: 'User'
