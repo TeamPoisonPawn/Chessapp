6b2cb40b9b0f41760e0ed2e44c40ccb13026eb54
Ryomablue <flclfan12@gmail.com>
2016-12-30 05:13:46 +0000

    Added is_obstructed method for vert/hor/diag moves

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,4 +9,47 @@ class Piece < ActiveRecord::Base
   #should have a side eg. white or black
   #functionality of all the unique pieces
   #how far pieces can move. special cases for all.
+
+  #determine if piece is obstructed to move
+  def is_obstructed?(x_destination, y_destination)
+    x_location = self.x_position
+    y_location = self.y_position
+    #check for vertical obstructions
+    if x_location == x_destination
+      y_location > y_destination ? incrementer = -1 : incrementer = 1
+      y_position = y_location + incrementer
+      while y_position != y_destination
+        if game.pieces.where(x_position: x_location, y_position: y_position).any?
+          return true
+        end
+        y_position += incrementer
+      end
+      return false
+    #check for horizontal obstructions
+    elsif y_location == y_destination
+      x_location > x_destination ? incrementer = -1 : incrementer = 1
+      x_position = x_location + incrementer
+      while x_position != x_destination
+        if game.pieces.where(y_position: y_location, x_position: x_position).any?
+          return true
+        end
+        x_position += incrementer
+      end
+      return false
+    #check for diagnol obstructions
+    elsif (x_location - x_destination).abs == (y_location - y_destination).abs
+      x_location > x_destination ? x_incrementer = -1 : x_incrementer = 1
+      y_location > y_destination ? y_incrementer = -1 : y_incrementer = 1
+      x_position = x_location + x_incrementer
+      y_position = y_location + y_incrementer
+      while x_position != x_destination && y_position != y_destination
+        if games.pieces.where(x_position: x_position, y_position: y_position).any?
+          return true
+        end
+        x_position += x_incrementer
+        y_position += y_incrementer
+      end
+      return false
+    end
+  end
 end
