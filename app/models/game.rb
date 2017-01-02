0a48350e15f0c36600b55e27e3fe73a8a4fa273f
Ryomablue <flclfan12@gmail.com>
2017-01-02 01:36:08 +0000

    Added comments about pieces x,y positions

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,17 +9,18 @@ class Game < ActiveRecord::Base
 end
 
   def initialize_board!
-    #white pawns (8)
-    #white rooks
-    #white bishops
-    #white knights
-    #white king
-    #white queen
+    #Will need player ID, game_id, color, x_pos, and y_pos
+    #white pawns (x_pos:0-7, y_pos:1)
+    #white rooks (left: 0,0 / right: 7,0)
+    #white knights (left: 1,0 / right: 6,0)
+    #white bishops (left: 2,0 / right: 5,0)
+    #white king (3,0)
+    #white queen (2,0)
 
-    #black pawns (8)
-    #black rooks
-    #black bishops
-    #black knights
-    #black king
-    #black queen
+    #black pawns (x_pos:0-7, y_pos:6)
+    #black rooks (left: 0,7 / right: 7,7)
+    #black knights (left: 1,7 / right: 6,7)
+    #black bishops (left: 2,7 / right: 5,7)
+    #black king (3,7)
+    #black queen (2,7)
   end
