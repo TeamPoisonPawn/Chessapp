cbbdbc8d7f30ec3ba736d19c0f93101db88ac80f
Chris Choi <cdddchris@gmail.com>
2017-01-03 06:29:16 +0000

    Added more logic for legal_move? Added parameters for it to pass in order for it to pass

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,22 +6,39 @@ class Piece < ActiveRecord::Base
     %w(Pawn Rook Bishop Knight Queen King)
   end
 
+  #If it passes all steps, move is valid
+  def valid_move?(x_destination, y_destination)
+    if nil_move?(x_destination, y_destination)
+      return false
+    elsif move_is_on_board?(x_destination, y_destination)
+      return false
+    elsif legal_move?(x_destination, y_destination)
+      return false
+    else is_obstructed?(x_destination, y_destination)
+      return false
+    end
+  end
+
   #The board size at maximum (x-axis, y-axis)
   min_size = 0
   max_size = 7
 
   #is the piece moving?
-  def move_is_nil?(x,y)
+  def move_is_nil?(x_destination, y_destination)
     x_location == x && y_location == y
   end
 
   #Check to see if the move exceeds the board size.
   #This is set by min_size & max_size
-  def move_is_on_board?(x,y)
+  def move_is_on_board?(x_destination, y_destination)
     (x <= max_size && x >= min_size) && (y <= max_size && y >= min_size)
   end
 
-  
+  #Can this piece move legally?
+  def legal_move?(_x, _y)
+    raise NotImplementError 'have #legal_move?'
+  end
+
   #should have a side eg. white or black
   #functionality of all the unique pieces
   #how far pieces can move. special cases for all.
