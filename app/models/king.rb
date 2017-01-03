d948ec6b602923b74cc950983c196895cc7ff21b
Chris Choi <cdddchris@gmail.com>
2017-01-03 07:30:08 +0000

    modified King.rb to check if movement is legal

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,4 +3,16 @@ class King < Piece
   # The king can only move one square in any direction - up, down, to the sides, and diagonally.
   # ** Special case **
   # The king may never move himself into check (where he could be captured).
+  def valid_move?(x_destination, y_destination)
+    allowed_movement?(x_destination, y_destination)
+  end
+  
+  private
+  #king movement is only at 1 so
+  def allowed_movement?(x_destination, y_destination)
+    x_movement_difference = (x_destination - x) #x needs to be changed to whatever we set the set params to
+    y_movement_difference = (y_destination - y)
+
+    (x_movement_difference <= 1) && (y_movement_difference <= 1)
+  end
 end
