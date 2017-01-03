92d5d0674cb2b073d33040d86c08fb97c0af14fa
Ryomablue <flclfan12@gmail.com>
2017-01-03 17:12:39 +0000

    Changed diagonal logic to else statement, created method to check if a piece is at destination

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -19,9 +19,6 @@ class Piece < ActiveRecord::Base
       y_location > y_destination ? incrementer = -1 : incrementer = 1
       y_position = y_location + incrementer
       while y_position != y_destination
-        if game.pieces.where(x_pos: x_location, y_pos: y_position).any?
-          return true
-        end
         y_position += incrementer
       end
       return false
@@ -30,26 +27,27 @@ class Piece < ActiveRecord::Base
       x_location > x_destination ? incrementer = -1 : incrementer = 1
       x_position = x_location + incrementer
       while x_position != x_destination
-        if game.pieces.where(y_pos: y_location, x_pos: x_position).any?
-          return true
-        end
         x_position += incrementer
       end
       return false
     #check for diagnol obstructions
-    elsif (x_location - x_destination).abs == (y_location - y_destination).abs
+    else 
+      raise error if(x_location - x_destination).abs != (y_location - y_destination).abs
       x_location > x_destination ? x_incrementer = -1 : x_incrementer = 1
       y_location > y_destination ? y_incrementer = -1 : y_incrementer = 1
       x_position = x_location + x_incrementer
       y_position = y_location + y_incrementer
       while x_position != x_destination && y_position != y_destination
-        if games.pieces.where(x_pos: x_position, y_pos: y_position).any?
-          return true
-        end
         x_position += x_incrementer
         y_position += y_incrementer
       end
       return false
+    end
+  end
+
+  def any_piece_blocking?
+    if games.pieces.where(x_pos: x_position, y_pos: y_position).any?
+      return true
     end
   end
 end
