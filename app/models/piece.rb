6ee669dccf55b114fa797357e8bdeb142a7c17e9
Ryomablue <flclfan12@gmail.com>
2017-01-06 22:05:41 +0000

    Fixed issues in is_obstructed method. Added new tests and all passed

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -19,6 +19,9 @@ class Piece < ActiveRecord::Base
       y_location > y_destination ? incrementer = -1 : incrementer = 1
       y_position = y_location + incrementer
       while y_position != y_destination
+        if game.pieces.where(x_pos: x_location, y_pos: y_position).any?
+          return true
+        end
         y_position += incrementer
       end
       return false
@@ -27,29 +30,36 @@ class Piece < ActiveRecord::Base
       x_location > x_destination ? incrementer = -1 : incrementer = 1
       x_position = x_location + incrementer
       while x_position != x_destination
+        if game.pieces.where(x_pos: x_position, y_pos: y_location).any?
+          return true
+        end
         x_position += incrementer
       end
       return false
     #check for diagnol obstructions
-    elsif 
+    else 
       (x_location - x_destination).abs != (y_location - y_destination).abs
       x_location > x_destination ? x_incrementer = -1 : x_incrementer = 1
       y_location > y_destination ? y_incrementer = -1 : y_incrementer = 1
       x_position = x_location + x_incrementer
       y_position = y_location + y_incrementer
       while x_position != x_destination && y_position != y_destination
+        if game.pieces.where(x_pos: x_position, y_pos: y_position).any?
+          return true
+        end
         x_position += x_incrementer
         y_position += y_incrementer
       end
       return false
-    else
-      raise error 'Invalid input'
     end
+    # else
+    #   raise error 'Invalid input'
+    # end
   end
 
-  def any_piece_blocking?
-    if games.pieces.where(x_pos: x_position, y_pos: y_position).any?
-      return true
-    end
-  end
+  # def any_piece_blocking?
+  #   if game.pieces.where(x_pos: x_position, y_pos: y_position).any?
+  #     return true
+  #   end
+  # end
 end
