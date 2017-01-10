7339b9a374079dca4c09c33557a6dbccae946e55
Ryomablue <flclfan12@gmail.com>
2017-01-10 22:22:23 +0000

    fixed is_obstruted tests and all passing

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Piece, type: :model do
     subject { piece.is_obstructed?(x_destination, y_destination) }
 
     context "right" do
-      let(:x_destination) { 7 }
+      let(:x_destination) { 6 }
       let(:y_destination) { 4 }
 
       it "should return false when no pieces on horizontal path" do
@@ -18,7 +18,7 @@ RSpec.describe Piece, type: :model do
       end
 
       it "should return true when another piece is on the horizontal path" do
-        FactoryGirl.create(:piece, x_pos: 6, y_pos: 4, game_id: game.id)
+        FactoryGirl.create(:piece, x_pos: 5, y_pos: 4, game_id: game.id)
         expect(subject).to be true 
       end
     end
@@ -39,56 +39,57 @@ RSpec.describe Piece, type: :model do
 
     context "up" do
       let(:x_destination) { 4 }
-      let(:y_destination) { 7 }
+      let(:y_destination) { 6 }
 
       it "should return false when no pieces on vertical path" do
         expect(subject).to be false
       end
 
       it "should return true when piece on vertical path" do
-        FactoryGirl.create(:piece, x_pos: 4, y_pos: 6, game_id: game.id)
+        FactoryGirl.create(:piece, x_pos: 4, y_pos: 5, game_id: game.id)
         expect(subject).to be true
       end
     end
 
     context "down" do
-      let(:x_destination) { 4 }
-      let(:y_destination) { 0 }
+      let(:piece) { FactoryGirl.create(:piece, x_pos: 5, y_pos: 5, game_id: game.id) }
+      let(:x_destination) { 5 }
+      let(:y_destination) { 3 }
 
       it "should return false when no pieces on vertical path" do
         expect(subject).to be false
       end
 
       it "should return true when another piece is on vertical path" do
-        FactoryGirl.create(:piece, x_pos: 4, y_pos: 1, game_id: game.id)
+        FactoryGirl.create(:piece, x_pos: 5, y_pos: 4, game_id: game.id)
         expect(subject).to be true
       end
     end
 
     context "up and right" do
-      let(:x_destination) { 7 }
-      let(:y_destination) { 7 }
+      let(:x_destination) { 6 }
+      let(:y_destination) { 6 }
 
-      it "should return flase when no pices are on diagonal path" do
+      it "should return false when no pieces are on diagonal path" do
         expect(subject).to be false
       end
 
       it "should return true when another piece is on vertical path" do
-        FactoryGirl.create(:piece, x_pos: 6, y_pos: 6, game_id: game.id)
+        FactoryGirl.create(:piece, x_pos: 5, y_pos: 5, game_id: game.id)
         expect(subject).to be true
       end
     end
 
     context "down and left" do
-      let(:x_destination) { 0 }
-      let(:y_destination) { 0 }
+      let(:x_destination) { 2 }
+      let(:y_destination) { 2 }
 
       it "should return false when no pieces are on diagonal path" do
         expect(subject).to be false
       end
 
       it "should return true when another piece is on diagonal path" do
-        FactoryGirl.create(:piece, x_pos: 2, y_pos: 2, game_id: game.id)
+        FactoryGirl.create(:piece, x_pos: 3, y_pos: 3, game_id: game.id)
         expect(subject).to be true
       end
     end
