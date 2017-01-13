8cca2432bb6deba89b4009d2b6e86bef29e2126b
Ryomablue <flclfan12@gmail.com>
2017-01-13 20:51:12 +0000

    fixed up queen logic

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -13,34 +13,28 @@ class Piece < ActiveRecord::Base
   min_size = 0
   max_size = 7
 
-  #is the piece moving?
-  # def move_is_nil?(x_destination, y_destination)
-  #   x_location == x && y_location == y
-  # end
+  # is the piece moving?
+  def move_is_nil?(x_destination, y_destination)
+    x_location == x && y_location == y
+  end
 
-  #Check to see if the move exceeds the board size.
-  #This is set by min_size & max_size
-  # def move_is_on_board?(x_destination, y_destination)
-  #   (x <= max_size && x >= min_size) && (y <= max_size && y >= min_size)
-  # end
+  # Check to see if the move exceeds the board size.
+  # This is set by min_size & max_size
+  def move_is_on_board?(x_destination, y_destination)
+    (x <= max_size && x >= min_size) && (y <= max_size && y >= min_size)
+  end
 
-  #Can this piece move legally?
-  # def legal_move?(_x, _y)
-  #   raise NotImplementError 'have #legal_move?'
-  # end
+  # Can this piece move legally?
+  def legal_move?(_x, _y)
+    raise NotImplementError 'have #legal_move?'
+  end
 
   #If it passes all steps, move is valid
   def valid_move?(x_destination, y_destination)
-    # if move_is_nil?(x_destination, y_destination)
-    #   return false
-    # if !move_is_on_board?(x_destination, y_destination)
-    #   return false
-    # elsif legal_move?(x_destination, y_destination)
-    #   return false
-    if is_obstructed?(x_destination, y_destination)
-      return false
-    end
-    true
+    !move_is_nil?(x_destination, y_destination) &&
+    move_is_on_board?(x_destination, y_destination) &&
+    !legal_move?(x_destination, y_destination) &&
+    !is_obstructed?(x_destination, y_destination)
   end
 
 
--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,14 +1,7 @@
 class Queen < Piece
   # She can move in any one straight direction - forward, backward, sideways,
   # or diagonally - as far as possible as long as she does not move through any of her own pieces.
-  # def initialize(args)
-  #   super
-  #   if args[:color] == 1
-  #     write_attribute(:piece_type, "white-queen.jpg")
-  #   else
-  #     write_attribute(:piece_type, "black-queen.jpg")
-  #   end
-  # end
+ 
 
   def can_move?(x_destionation, y_destination)
     return false unless valid_move?(x_destionation, y_destination)
