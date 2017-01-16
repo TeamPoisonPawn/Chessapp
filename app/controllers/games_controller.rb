6138967efe551fbfa67478acff2289804d2c5a34
Christian Maddox <cmaddox5@vols.utk.edu>
2017-01-16 01:14:23 +0000

    Added the update action for Game. Added link to join on home page.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,25 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game.update_attributes(black_player_id: current_user.id) if !@game.black_player_id
+    @game.update_attributes(white_player_id: current_user.id) if !@game.white_player_id
+  end
+
+  private
+
+  def render_not_found
+    render text: 'Not Found', status: :not_found
+  end
+
+  def find_game
+    begin
+      @game = Game.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      return render_not_found
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home_page#index'
   resources :pieces, only: [:show, :update]
-  resources :games, only: :show
+  resources :games, only: [:show, :update]
 end
