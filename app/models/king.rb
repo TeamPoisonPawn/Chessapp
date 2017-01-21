5af4a0eb826d996542b644ae43a6000d27c94ffe
Christian Maddox <cmaddox5@vols.utk.edu>
2017-01-21 03:06:51 +0000

    Changed king.rb to inherit valid_move? and make a super call.

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,16 +3,15 @@ class King < Piece
   # The king can only move one square in any direction - up, down, to the sides, and diagonally.
   # ** Special case **
   # The king may never move himself into check (where he could be captured).
-  def legal_move?(x_destination, y_destination)
-    allowed_movement?(x_destination, y_destination)
+  def valid_move?(x_destination, y_destination)
+    super && legal_move?(x_destination, y_destination)
   end
 
-  private
   #king movement is only at 1 so when x_destination must be -1 from the location of the king(params)
-  def allowed_movement?(x_destination, y_destination)
-    x_movement_difference = (x_destination - x) #x needs to be changed to whatever we set the set params to
-    y_movement_difference = (y_destination - y)
+  def legal_move?(x_destination, y_destination)
+    x_movement_difference = (x_destination - self.x_pos).abs
+    y_movement_difference = (y_destination - self.y_pos).abs
 
-    (x_movement_difference <= 1) && (y_movement_difference <= 1)
+    x_movement_difference.between?(0, 1) && y_movement_difference.between?(0, 1)
   end
 end
