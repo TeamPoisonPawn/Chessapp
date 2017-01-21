b7b5293f71c9eb015ccc37e3909ec8fce9c23a0f
Christian Maddox <cmaddox5@vols.utk.edu>
2017-01-21 01:10:57 +0000

    Set up a link for users to join an open game if they are not in it.
    Added a link on the home page for players to click if they want to join
    an open game. The player cannot already be in the game. Set up some
    tests for the update action in GamesController.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,21 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_game, :only => [:show, :update]
 
   def show
   end
 
   def update
-    @game.update_attributes(black_player_id: current_user.id) if !@game.black_player_id
-    @game.update_attributes(white_player_id: current_user.id) if !@game.white_player_id
+    if @game.black_player_id.nil? && @game.white_player_id != current_user.id
+      @game.update_attributes(black_player_id: current_user.id)
+      render "show"
+    elsif @game.white_player_id.nil? && @game.black_player_id != current_user.id
+      @game.update_attributes(white_player_id: current_user.id)
+      render "show"
+    else
+      flash.now[:alert] = "You are already a player in this game!"
+      render "show"
+    end
   end
 
   private
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "game#show action" do
+    it "should successfully display the board if the game is found" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      game = FactoryGirl.create(:game)
+      get :show, id: game.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the game is not found" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      get :show, id: 'notanid'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "game#update action" do
+    it "should successfully update the game's white_player_id if the user is not already in the game" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      game = FactoryGirl.create(:game, black_player_id: 1, white_player_id: nil)
+      put :update, id: game.id
+      game.reload
+      expect(response).to redirect_to game_path(game.id)
+      expect(game.white_player_id).to eq user.id
+    end
+  end
 
 end
