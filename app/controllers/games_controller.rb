732a8b3c36360a8d276aac24742355cade0cc685
Chris Choi <cdddchris@gmail.com>
2017-01-26 04:58:03 +0000

    added some params and am changing king.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,11 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def game_params
+    params.require(:game).permit(
+      :name, :black_player_id, :white_player_id, :users, :status, :player_turn_id,
+      :castle_w_kingside, :castle_w_queenside, :castle_b_kingside, :castle_b_queenside
+    )
+  end
 end
